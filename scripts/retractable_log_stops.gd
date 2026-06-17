72f8ad0f703428f5feb8cf99c3a4e15887ac81ca
Facesnatcher <Facesnatcher56@gmail.com>
2026-06-17 17:55:35 -0400

    Redesign log stops to orange rotating stop-and-loader crescent arm assembly

--- scripts/retractable_log_stops.gd
+++ scripts/retractable_log_stops.gd
@@ -9,9 +9,9 @@ enum StopState {
 	EXTENDING,
 }
 
-@export var extended_y: float = 1.28
-@export var retracted_y: float = 0.72
-@export var move_speed: float = 1.8
+@export var extended_rotation_deg: float = 0.0
+@export var retracted_rotation_deg: float = -70.0
+@export var rotation_speed: float = 120.0 # degrees per second
 @export var hold_time: float = 0.45
 @export var retracted_time: float = 1.25
 
@@ -20,20 +20,22 @@ enum StopState {
 
 var _state: StopState = StopState.EXTENDED
 var _timer: float = 0.0
+var _current_rot: float = 0.0
 
 func _ready() -> void:
+	_current_rot = extended_rotation_deg
 	if Engine.is_editor_hint():
 		if moving_stops:
-			moving_stops.position.y = extended_y
+			moving_stops.rotation.x = deg_to_rad(_current_rot)
 		return
 		
-	moving_stops.position.y = extended_y
+	moving_stops.rotation.x = deg_to_rad(_current_rot)
 	trigger_area.body_entered.connect(_on_trigger_area_body_entered)
 
 func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
-		if moving_stops and moving_stops.position.y != extended_y:
-			moving_stops.position.y = extended_y
+		if moving_stops and moving_stops.rotation.x != deg_to_rad(extended_rotation_deg):
+			moving_stops.rotation.x = deg_to_rad(extended_rotation_deg)
 		return
 		
 	match _state:
@@ -42,8 +44,8 @@ func _physics_process(delta: float) -> void:
 			if _timer <= 0.0:
 				_state = StopState.RETRACTING
 		StopState.RETRACTING:
-			_move_stops_toward(retracted_y, delta)
-			if is_equal_approx(moving_stops.position.y, retracted_y):
+			_move_stops_toward(retracted_rotation_deg, delta)
+			if is_equal_approx(_current_rot, retracted_rotation_deg):
 				_state = StopState.RETRACTED
 				_timer = retracted_time
 		StopState.RETRACTED:
@@ -55,13 +57,13 @@ func _physics_process(delta: float) -> void:
 			if not has_log:
 				_state = StopState.EXTENDING
 		StopState.EXTENDING:
-			_move_stops_toward(extended_y, delta)
-			if is_equal_approx(moving_stops.position.y, extended_y):
+			_move_stops_toward(extended_rotation_deg, delta)
+			if is_equal_approx(_current_rot, extended_rotation_deg):
 				_state = StopState.EXTENDED
 
-func _move_stops_toward(target_y: float, delta: float) -> void:
-	var current := moving_stops.position.y
-	moving_stops.position.y = move_toward(current, target_y, move_speed * delta)
+func _move_stops_toward(target_rot: float, delta: float) -> void:
+	_current_rot = move_toward(_current_rot, target_rot, rotation_speed * delta)
+	moving_stops.rotation.x = deg_to_rad(_current_rot)
 
 func _on_trigger_area_body_entered(body: Node3D) -> void:
 	if _state != StopState.EXTENDED:
@@ -69,3 +71,4 @@ func _on_trigger_area_body_entered(body: Node3D) -> void:
 	if body is RigidBody3D and body.is_in_group("logs"):
 		_state = StopState.HOLDING_LOG
 		_timer = hold_time
+
